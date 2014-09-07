4e82e240d20a513961a1387ef1dfb99b785cce93
decolua <decoluadt@gmail.com>
2014-09-07 12:50:57 +0700

    Delete All

--- src/com/deco/config/DATABASE.java
+++
@@ -1,5 +0,0 @@
-package com.deco.config;
-
-public class DATABASE {
-	public static int VERSION = 301;
-}
--- src/com/deco/config/SERVER.java
+++
@@ -1,7 +0,0 @@
-package com.deco.config;
-
-public class SERVER {
-	public static String HOST = "http://footballchallenger.net";
-	public static String SERVICE_URI = "service.php";
-	public static String SERVICE_URL = HOST + "/" + SERVICE_URI + "?";
-}
